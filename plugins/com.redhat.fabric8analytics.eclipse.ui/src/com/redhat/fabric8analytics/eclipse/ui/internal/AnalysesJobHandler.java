dba5fb0591facda67138c7e404a50f4251f11f57
Rastislav Wagner <rawagner@redhat.com>
2018-04-17 10:28:43 +0200

    Add tabs to StackAnalysis view
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>

--- plugins/com.redhat.fabric8analytics.eclipse.ui/src/com/redhat/fabric8analytics/eclipse/ui/internal/AnalysesJobHandler.java
+++ plugins/com.redhat.fabric8analytics.eclipse.ui/src/com/redhat/fabric8analytics/eclipse/ui/internal/AnalysesJobHandler.java
@@ -14,6 +14,7 @@ package com.redhat.fabric8analytics.eclipse.ui.internal;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.FileLocator;
@@ -21,11 +22,11 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 import com.redhat.fabric8analytics.eclipse.ui.internal.pom.EditorComposite;
 import com.redhat.fabric8analytics.eclipse.ui.internal.view.StackAnalysesView;
-import com.redhat.fabric8analytics.eclipse.ui.internal.view.StackAnalysisHandler;
 import com.redhat.fabric8analytics.eclipse.ui.Fabric8AnalysisUIActivator;
 import com.redhat.fabric8analytics.eclipse.core.api.RecommenderAPIProvider;
 import com.redhat.fabric8analytics.eclipse.core.job.RetrieveResultsJob;
@@ -40,10 +41,11 @@ public class AnalysesJobHandler {
 	private Set<IFile> pomFiles;
 
 	private EditorComposite editorComposite;
-	
+
 	private IFile license;
 
-	public AnalysesJobHandler(RecommenderAPIProvider provider, Set<IFile> pomFiles, IFile license, EditorComposite editorComposite) {
+	public AnalysesJobHandler(RecommenderAPIProvider provider, Set<IFile> pomFiles, IFile license,
+			EditorComposite editorComposite) {
 		this.provider = provider;
 		this.pomFiles = pomFiles;
 		this.editorComposite = editorComposite;
@@ -51,17 +53,28 @@ public class AnalysesJobHandler {
 	}
 
 	public void analyze() {
-		URL url;
+		final URL url;
 		try {
-			url = new URL("platform:/plugin/"+Fabric8AnalysisUIActivator.PLUGIN_ID+"/templates/index.html");
-			url = FileLocator.toFileURL(url);
-		} catch (IOException e ) {
+			url = FileLocator.toFileURL(
+					new URL("platform:/plugin/" + Fabric8AnalysisUIActivator.PLUGIN_ID + "/templates/index.html"));
+		} catch (IOException e) {
 			Fabric8AnalysisUIActivator.getDefault().logError("Error while retrieving browser template", e);
 			return;
 		}
 		if (editorComposite == null) {
-			IViewPart mainView = StackAnalysisHandler.getView();
-			((StackAnalysesView) mainView).updatebrowserUrl(url.toString());
+			Display.getDefault().syncExec(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						StackAnalysesView view = (StackAnalysesView) PlatformUI.getWorkbench()
+								.getActiveWorkbenchWindow().getActivePage().showView(StackAnalysesView.NAME);
+						view.openProjectAnalysis(pomFiles.stream().map(IFile::getProject).collect(Collectors.toList()), url.toString());
+					} catch (PartInitException e) {
+						MessageDialogUtils.displayErrorMessage("Error while opening stack analysis view", e);
+					}
+				}
+			});
+
 		} else {
 			editorComposite.updateBrowser(url.toString());
 		}
@@ -72,27 +85,28 @@ public class AnalysesJobHandler {
 
 			@Override
 			public void done(IJobChangeEvent event) {
-				if(event.getResult().isOK()) {
-					RetrieveResultsJob retrieveResultsJob = new RetrieveResultsJob(provider, sentToAnalysisJob.getJobID());
+				if (event.getResult().isOK()) {
+					RetrieveResultsJob retrieveResultsJob = new RetrieveResultsJob(provider,
+							sentToAnalysisJob.getJobID());
 					retrieveResultsJob.schedule();
-					
+
 					retrieveResultsJob.addJobChangeListener(new JobChangeAdapter() {
-						
+
 						private int numOfRequests = 0;
-						
+
 						@Override
 						public void done(IJobChangeEvent event) {
-							if(numOfRequests == 5) {
+							if (numOfRequests == 5) {
 								disposeBrowsers();
 								MessageDialogUtils.displayErrorMessage("Analysis timeout");
 								return;
 							}
 							numOfRequests++;
-							if(event.getResult().getSeverity() == IStatus.INFO) {
-								//reschedule if analysis is not yet complete
+							if (event.getResult().getSeverity() == IStatus.INFO) {
+								// reschedule if analysis is not yet complete
 								retrieveResultsJob.schedule(TIMER_INTERVAL);
-							} else if(event.getResult().isOK()) {
-								//analysis finished, get the result
+							} else if (event.getResult().isOK()) {
+								// analysis finished, get the result
 								syncWithUi(retrieveResultsJob.getAnalysisURL());
 							}
 						}
@@ -106,7 +120,13 @@ public class AnalysesJobHandler {
 		Display.getDefault().asyncExec(new Runnable() {
 			public void run() {
 				if (editorComposite == null) {
-					((StackAnalysesView) StackAnalysisHandler.getView()).updatebrowserUrl(analysisURL);
+					try {
+						StackAnalysesView view = (StackAnalysesView) PlatformUI.getWorkbench()
+								.getActiveWorkbenchWindow().getActivePage().showView(StackAnalysesView.NAME);
+						view.openProjectAnalysis(pomFiles.stream().map(IFile::getProject).collect(Collectors.toList()), analysisURL);
+					} catch (PartInitException e) {
+						MessageDialogUtils.displayErrorMessage("Error while opening stack analysis view", e);
+					}
 				} else {
 					editorComposite.updateBrowser(analysisURL);
 				}
@@ -116,10 +136,16 @@ public class AnalysesJobHandler {
 	}
 
 	private void disposeBrowsers() {
-		Display.getDefault().syncExec(new Runnable () {
+		Display.getDefault().syncExec(new Runnable() {
 			public void run() {
-				if(editorComposite == null) {
-					((StackAnalysesView) StackAnalysisHandler.getView()).disposebrowserUrl();
+				if (editorComposite == null) {
+					try {
+						StackAnalysesView view = (StackAnalysesView) PlatformUI.getWorkbench()
+								.getActiveWorkbenchWindow().getActivePage().showView(StackAnalysesView.NAME);
+						view.disposeBrowser(pomFiles.stream().map(IFile::getProject).collect(Collectors.toList()));
+					} catch (PartInitException e) {
+						MessageDialogUtils.displayErrorMessage("Error while opening stack analysis view", e);
+					}
 				} else {
 					editorComposite.disposeBrowser();
 				}
--- plugins/com.redhat.fabric8analytics.eclipse.ui/src/com/redhat/fabric8analytics/eclipse/ui/internal/view/StackAnalysesView.java
+++ plugins/com.redhat.fabric8analytics.eclipse.ui/src/com/redhat/fabric8analytics/eclipse/ui/internal/view/StackAnalysesView.java
@@ -11,9 +11,18 @@
 
 package com.redhat.fabric8analytics.eclipse.ui.internal.view;
 
-import org.eclipse.jface.layout.GridDataFactory;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.resources.IProject;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
@@ -21,36 +30,69 @@ import org.eclipse.ui.part.ViewPart;
 public class StackAnalysesView extends ViewPart {
 
 	public static final String NAME = "com.redhat.fabric8analytics.eclipse.ui.StackAnalysesView";
-	
-	private static Browser browser = null;
+
+	private CTabFolder folder;
+	private Map<List<IProject>, CTabItem> openAnalysis = new HashMap<>();
 
 	@Override
 	public void createPartControl(Composite parent) {
-		browser = new Browser(parent, SWT.NONE);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(browser);	
+		folder = new CTabFolder(parent, SWT.BOTTOM);
+	}
+
+	public void openProjectAnalysis(List<IProject> projects, String browserUrl) {
+		if (!openAnalysis.containsKey(projects)) {
+			Display.getDefault().asyncExec(new Runnable() {
+				
+				@Override
+				public void run() {
+					CTabItem item = new CTabItem(folder, SWT.CLOSE);
+					item.setText(projects.stream().map(IProject::getName).collect(Collectors.joining(",")));
+					Browser browser = new Browser(folder, SWT.NONE);
+					item.setControl(browser);
+					item.addDisposeListener(new DisposeListener() {
+						
+						@Override
+						public void widgetDisposed(DisposeEvent e) {
+							openAnalysis.remove(projects);
+						}
+					});
+					browser.setUrl(browserUrl);
+					openAnalysis.put(projects, item);
+					folder.setSelection(folder.indexOf(item));
+				}
+			});
+		} else {
+			CTabItem item = openAnalysis.get(projects);
+			Browser browser = (Browser)item.getControl();
+			Display.getDefault().asyncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					browser.setUrl(browserUrl);
+					folder.setSelection(folder.indexOf(item));
+				}
+				
+			});
+		}
+	}
+
+	public void disposeBrowser(List<IProject> projects) {
+		if (openAnalysis.containsKey(projects)) {
+			Display.getDefault().asyncExec(new Runnable() {
+				public void run() {
+					CTabItem item = openAnalysis.get(projects);
+					Browser browser = (Browser)item.getControl();
+					if (!browser.isDisposed()) {
+						browser.dispose();
+					}
+				}
+			});
+		}
 	}
 
 	@Override
 	public void setFocus() {
-		browser.setFocus();
+		folder.setFocus();
 	}
 
-	public void updatebrowserUrl(String browseUrl) {
-		Display.getDefault().asyncExec(new Runnable(){
-			public void run(){
-				if (!browser.isDisposed()) {
-					browser.setUrl(browseUrl);
-				}
-			}
-		});
-	}
-	public void disposebrowserUrl() {
-		Display.getDefault().asyncExec(new Runnable(){
-			public void run(){
-				if (!browser.isDisposed()) {
-					browser.dispose();;
-				}
-			}
-		});
-	}
 }
